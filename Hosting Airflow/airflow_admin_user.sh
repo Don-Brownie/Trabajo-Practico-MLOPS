37ef1f14681ca3c0223944a5e09a9eb55ec3e051
Don-Brownie <bmalarini@gmail.com>
2024-11-21 20:20:18 -0300

    Update airflow_admin_user.sh

--- Hosting Airflow/airflow_admin_user.sh
+++ Hosting Airflow/airflow_admin_user.sh
@@ -1,8 +1,9 @@
 #Está estilizado para facilidad de lectura, pero este código es el que realmente cambia el nombre del usuario maestro de airflow
 
 airflow users create \
-    --username admin \         # Username for the admin account
-    --firstname Ad \           # First name of the admin user
-    --lastname Min \           # Last name of the admin user
-    --role Admin \             # Role assigned to the user (Admin)
-    --email ad@min.com         # Email address of the admin user
+    --username admin \          # Username for the admin account
+    --firstname Ad \            # First name of the admin user
+    --lastname Min \            # Last name of the admin user
+    --role Admin \              # Role assigned to the user (Admin)
+    --email ad@min.com \        # Email address of the admin user
+    --password yourpassword123  # Password for the admin user
