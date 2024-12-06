f0e1c882fdc74ce9d431be5948c2ee630611eb28
Don-Brownie <bmalarini@gmail.com>
2024-12-05 23:26:46 -0300

    Update airflow_config.sh

--- Hosting Airflow/airflow_config.sh
+++ Hosting Airflow/airflow_config.sh
@@ -1,1 +1,10 @@
 sed -i 's/^load_examples = True/load_examples = False/' "$AIRFLOW_CFG_PATH"
+
+# Define the path to your airflow.cfg file (you may need to adjust this path)
+AIRFLOW_CFG_PATH="$HOME/airflow/airflow.cfg"
+
+# Define the new path for the dags_folder
+NEW_DAGS_FOLDER="/home/ubuntu/Trabajo-Practico-MLOPS/'Hosting Airlfow'/DAGs"
+
+# Use sed to replace the dags_folder path
+sed -i "s|^dags_folder = .*|dags_folder = $NEW_DAGS_FOLDER|" "$AIRFLOW_CFG_PATH"
