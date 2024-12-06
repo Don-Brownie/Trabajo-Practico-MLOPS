c946982a9278ac18aa655995aa50c25018c783b4
isoldasalas <isoldasalas1@gmail.com>
2024-12-06 11:19:05 -0300

    Update Top_CTR.py

--- Data Filters/Top_CTR.py
+++ Data Filters/Top_CTR.py
@@ -34,3 +34,19 @@ def calcular_top_ctr(ads_views, top_n=20):
     )
 
     return top_ctr[['advertiser_id', 'product_id', 'ctr']]
+
+
+def run_top_ctr():
+    # Leer datos filtrados desde EC2
+    filtered_ads = pd.read_csv('/tmp/filtered_ads.csv')
+    top_ctr = (
+        filtered_ads[filtered_ads['type'] == 'click']
+        .groupby(['advertiser_id', 'product_id'])
+        .size()
+        .reset_index(name='click_count')
+        .sort_values(by='click_count', ascending=False)
+        .groupby('advertiser_id')
+        .head(20)
+    )
+    save_to_ec2(top_ctr, '/tmp/top_ctr.csv')
+
