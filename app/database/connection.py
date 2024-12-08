dbb30921c1c80f957ad57704cf553e1fe4ac5676
luanagiusto <giusto.luana@gmail.xom>
2024-12-08 18:47:04 -0300

    Cambios en la contraseña de acceso

--- app/database/connection.py
+++ app/database/connection.py
@@ -6,6 +6,6 @@ def get_connection():
         host= "grupo-17-rds.cf4i6e6cwv74.us-east-1.rds.amazonaws.com",
         database= "grupo-17-rds",
         user= "postgres",
-        password= "postgres",
+        password= "yourpassword123",
         cursor_factory=RealDictCursor
     )
