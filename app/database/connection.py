93aaf7624b724d265cd9f00350c899e0cd635430
luagiusto <93746698+luagiusto@users.noreply.github.com>
2024-12-08 17:49:17 -0300

    Update connection.py
    las credenciales van como string

--- app/database/connection.py
+++ app/database/connection.py
@@ -3,9 +3,9 @@ from psycopg2.extras import RealDictCursor
 
 def get_connection():
     return psycopg2.connect(
-        host= grupo-17-rds.cf4i6e6cwv74.us-east-1.rds.amazonaws.com,
-        database= grupo-17-rds,
-        user= postgres,
-        password= postgres,
+        host= "grupo-17-rds.cf4i6e6cwv74.us-east-1.rds.amazonaws.com",
+        database= "grupo-17-rds",
+        user= "postgres",
+        password= "postgres",
         cursor_factory=RealDictCursor
     )
