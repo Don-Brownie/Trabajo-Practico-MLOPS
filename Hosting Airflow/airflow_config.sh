e2f940c59d34037587e06fe07ead2c97550c2daa
riosagustinav <162245211+riosagustinav@users.noreply.github.com>
2024-12-07 21:37:46 -0300

    Update the Airflow configuration file to use the LocalExecutor and set parallelism to 2

--- Hosting Airflow/airflow_config.sh
+++ Hosting Airflow/airflow_config.sh
@@ -4,3 +4,6 @@ AIRFLOW_CFG_PATH="$HOME/airflow/airflow.cfg"
 # Update 'load_examples' to False
 sed -i 's/^load_examples = True/load_examples = False/' "$AIRFLOW_CFG_PATH"
 
+# Update the Airflow configuration file to use the LocalExecutor and set parallelism to 2
+sed -i 's/^executor = SequentialExecutor/executor = LocalExecutor/' "$AIRFLOW_CFG_PATH"
+sed -i 's/^parallelism = 32/parallelism = 2/' "$AIRFLOW_CFG_PATH"
